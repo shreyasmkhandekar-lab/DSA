8e6a9826c409671e0e917846400700795678ba5d
Shreyas Khandekar <shreyasmkhandekar@gmail.com>
2026-03-25 21:36:51 +0530

    Linked List methods

---
+++ LL/Main.java
@@ -0,0 +1,20 @@
+package LL;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // methods for linked list
+
+        // LinkedList myLinkedList = new LinkedList(0);
+        // myLinkedList.append(1);
+        // myLinkedList.prepend(3);
+        // myLinkedList.get(1);
+        // myLinkedList.set(1, 3);
+        // myLinkedList.insert(1, 9);
+        // myLinkedList.removeFirst();
+        // myLinkedList.removeLast();
+        // myLinkedList.remove(1);
+        // myLinkedList.reverse();
+
+    }
+}
