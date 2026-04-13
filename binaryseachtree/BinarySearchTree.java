007512f948663bcbecee61643e0f8ebea5f3b1df
shreyasmkhandekar-lab <shreyasmkhandekar@gmail.com>
2026-04-13 02:32:17 -0400

    Recursive Binary Search Tree

--- binaryseachtree/BinarySearchTree.java
+++ binaryseachtree/BinarySearchTree.java
@@ -52,4 +52,77 @@ public class BinarySearchTree {
         }
         return false;
     }
+
+//  ************************ Recursive Methods **************************** //
+
+    private boolean rContains(Node currentNode, int value){
+    if(currentNode == null) return false;
+
+    if(currentNode.value == value) return true;
+
+    if(currentNode.value > value){
+        return rContains(currentNode.left, value);
+    }else{
+        return rContains(currentNode.right, value);
+      }
+    }
+
+    public boolean rContains(int value){
+        return rContains(root, value);
+    }
+
+    private Node rInsert(Node currentNode, int value){
+        if(currentNode == null) return new Node(value);
+
+        if(value < currentNode.value){
+            currentNode.left = rInsert(currentNode.left, value);
+        }else if(value > currentNode.value){
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    public void rInsert(int value){
+        if(root == null) root = new Node(value);
+        rInsert(root,value); 
+    }
+
+    public int minValue(Node currentNode){
+        while(currentNode.left != null){
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    private Node rDeleteNode(Node currentNode, int value){
+        if(currentNode == null) return null;
+
+        if(value < currentNode.value){
+            currentNode.left = rDeleteNode(currentNode.left, value);
+        }
+        else if(value > currentNode.value){
+            currentNode.right = rDeleteNode(currentNode.right, value);
+        }
+        else{
+            if(currentNode.left== null && currentNode.right == null){
+                return null;
+            }
+            else if(currentNode.left == null){
+                currentNode = currentNode.right;
+            }
+            else if(currentNode.right == null){
+                currentNode = currentNode.left;
+            }
+            else{
+                int subTreeMin = minValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                currentNode.right = rDeleteNode(currentNode.right, subTreeMin);
+            }
+        }
+        return currentNode;
+    }
+
+    public void rDeleteNode(int value){
+        rDeleteNode(root, value);
+    }
 }
--- binaryseachtree/Main.java
+++ binaryseachtree/Main.java
@@ -4,21 +4,24 @@ public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
 
-        myBST.insert(30);
-        myBST.insert(15);
-        myBST.insert(56);
-        myBST.insert(12);
-        myBST.insert(77);
-        myBST.insert(5);
-        myBST.insert(50);
+        myBST.rInsert(2);
+        myBST.rInsert(1);
+        myBST.rInsert(3);
 
-        myBST.insert(27);
+        myBST.rDeleteNode(2);
 
         // System.out.println(myBST.root.left.left.left.value); // 5
 
-        System.out.println(myBST.contains(56));
-        System.out.println(myBST.contains(27));
-        System.out.println(myBST.contains(29));
+        // System.out.println(myBST.contains(56));
+        // System.out.println(myBST.contains(27));
+        // System.out.println(myBST.contains(29));
+
+        // System.out.println(myBST.rContains(27));
+        // System.out.println(myBST.rContains(10));
+
+        System.out.println(myBST.root.value);
+        System.out.println(myBST.root.left.value);
+        System.out.println(myBST.root.right);
     }
 }
 
