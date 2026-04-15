5cc226f785f936775f4241d488ebc066663593ce
shreyasmkhandekar-lab <shreyasmkhandekar@gmail.com>
2026-04-15 01:16:10 -0400

    BFS

---
+++ treetraversal/Main.java
@@ -0,0 +1,17 @@
+package treetraversal;
+
+public class Main {
+    public static void main(String[] args) {
+        binarysearchtree myBST = new binarysearchtree();
+
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(27);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        System.out.println(myBST.BFS());
+    }
+}
---
+++ treetraversal/binarysearchtree.java
@@ -0,0 +1,161 @@
+package treetraversal;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class binarysearchtree {
+    Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value){
+            this.value = value;
+        }
+    }
+
+//  ************************ Tree Traversal **************************** //
+
+    public ArrayList<Integer> BFS(){
+        Node currentNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        queue.add(currentNode);
+
+        while(queue.size() > 0){
+            currentNode = queue.remove();
+            result.add(currentNode.value);
+            if(currentNode.left != null){
+                queue.add(currentNode.left);
+            }
+            if(currentNode.right != null){
+                queue.add(currentNode.right);
+            }
+        }
+        return result;
+    }
+
+
+
+
+
+
+//  ************************ Regular Methods **************************** //
+
+
+    public boolean insert(int value){
+        Node newnNode = new Node(value);
+        if(root == null){
+            root = newnNode;
+            return true;
+        }
+        Node temp = root;
+        while(true){
+            if(newnNode.value == temp.value) return false;
+            if(newnNode.value < temp.value){
+                if(temp.left == null){
+                    temp.left = newnNode;
+                    return true;
+                }
+                temp = temp.left;
+            }else{
+                if(temp.right == null){
+                    temp.right = newnNode;
+                    return true;
+                }
+                temp = temp.right;
+            }
+        }
+    }
+
+    public boolean contains(int value){
+        if(root == null) return false;
+        Node temp = root;
+        while(temp != null){
+            if(value < temp.value){
+                temp = temp.left;
+            }else if(value > temp.value){
+                temp = temp.right;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+//  ************************ Recursive Methods **************************** //
+
+    private boolean rContains(Node currentNode, int value){
+    if(currentNode == null) return false;
+
+    if(currentNode.value == value) return true;
+
+    if(currentNode.value > value){
+        return rContains(currentNode.left, value);
+    }else{
+        return rContains(currentNode.right, value);
+      }
+    }
+
+    public boolean rContains(int value){
+        return rContains(root, value);
+    }
+
+    private Node rInsert(Node currentNode, int value){
+        if(currentNode == null) return new Node(value);
+
+        if(value < currentNode.value){
+            currentNode.left = rInsert(currentNode.left, value);
+        }else if(value > currentNode.value){
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+
+    public void rInsert(int value){
+        if(root == null) root = new Node(value);
+        rInsert(root,value); 
+    }
+
+    public int minValue(Node currentNode){
+        while(currentNode.left != null){
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    private Node rDeleteNode(Node currentNode, int value){
+        if(currentNode == null) return null;
+
+        if(value < currentNode.value){
+            currentNode.left = rDeleteNode(currentNode.left, value);
+        }
+        else if(value > currentNode.value){
+            currentNode.right = rDeleteNode(currentNode.right, value);
+        }
+        else{
+            if(currentNode.left== null && currentNode.right == null){
+                return null;
+            }
+            else if(currentNode.left == null){
+                currentNode = currentNode.right;
+            }
+            else if(currentNode.right == null){
+                currentNode = currentNode.left;
+            }
+            else{
+                int subTreeMin = minValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                currentNode.right = rDeleteNode(currentNode.right, subTreeMin);
+            }
+        }
+        return currentNode;
+    }
+
+    public void rDeleteNode(int value){
+        rDeleteNode(root, value);
+    }
+}
