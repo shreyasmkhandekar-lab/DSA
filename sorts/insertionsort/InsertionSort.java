53f663e7674af1a4b77080e1f0836e63ceefa4d9
shreyasmkhandekar-lab <shreyasmkhandekar@gmail.com>
2026-04-16 00:47:50 -0400

    Insertion Sort

---
+++ sorts/insertionsort/InsertionSort.java
@@ -0,0 +1,25 @@
+package sorts.insertionsort;
+
+import java.util.Arrays;
+
+public class InsertionSort {
+    public static void insertionSort(int[] array){
+        for(int i=1;i<array.length;i++){
+            int temp = array[i];
+            int j = i - 1;
+            while(j > -1 && temp < array[j]){
+                array[j+1] = array[j];
+                array[j]  = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] myArray = {1, 2, 9, 6,4,3};
+
+        insertionSort(myArray);
+
+        System.out.println(Arrays.toString(myArray));
+    }
+}
